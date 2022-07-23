d1005f0fb9fcaf387a91e57fd9a0fd9254086b9d
Albert Hidalgo <alhidalgodev@gmail.com>
2022-07-22 20:26:10 -0400

    feat
    * Handle errors

--- src/controllers/MessagesController.js
+++ src/controllers/MessagesController.js
@@ -3,23 +3,29 @@ import MessageService from '../services/MessagesService';
 class MessageController {
 
   static async sendMessage(req, res) {
-    const phone = req.body.phone;
-    const message = req.body.message;
+    try {
+      const phone = req.body.phone;
+      const message = req.body.message;
 
-    if (!phone || !message) {
-      res.status(400).json({
-        status: 400,
-        error: "Missing parameters"
+      if (!phone || !message) {
+        res.status(400).json({
+          status: 400,
+          error: "Missing parameters"
+        });
+        return;
+      }
+
+      await MessageService.sendMessage({ to: phone, message });
+      res.status(200).json({
+        status: 200,
+        message: "Message sent"
       });
-      return;
+    } catch (error) {
+      res.status(500).json({
+        status: 500,
+        error: error.message
+      });
     }
-
-    await MessageService.sendMessage({ to: phone, message });
-    res.status(200).json({
-      status: 200,
-      message: "Message sent"
-    });
-  
   }
 
 }
--- src/services/MessagesService.js
+++ src/services/MessagesService.js
@@ -4,21 +4,27 @@ class MessagesService {
   static async sendMessage({to, message}) {
     const token = process.env.WHATSAPP_TOKEN;
     const phoneNumberId = process.env.WHATSAPP_PHONE_ID;
-    
-    axios({
-      method: "POST",
-      url:
-        "https://graph.facebook.com/v12.0/" +
-        phoneNumberId +
-        "/messages?access_token=" +
-        token,
-      data: {
-        messaging_product: "whatsapp",
-        to: to,
-        text: { body: message },
-      },
-      headers: { "Content-Type": "application/json" },
-    });
+    try {
+      return axios({
+        method: "POST",
+        url:
+          "https://graph.facebook.com/v12.0/" +
+          phoneNumberId +
+          "/messages?access_token=" +
+          token,
+        data: {
+          messaging_product: "whatsapp",
+          to: to,
+          text: { body: message },
+        },
+        headers: { "Content-Type": "application/json" },
+      })
+      .catch(() => {
+        throw { message: 'Error to send message' };
+      })
+    } catch (error) {
+      throw error;
+    }
   }
 }
 
