7b0553aa1048a16348784c2e79efd8d7110184da
Albert Hidalgo <alhidalgodev@gmail.com>
2022-07-30 17:30:19 -0400

    feat
    * Added message template router

--- src/routes/messages.js
+++ src/routes/messages.js
@@ -5,5 +5,6 @@ import { verifyToken } from '../middlewares/Auth';
 const router = Router();
 
 router.post('/send-message', verifyToken, MessageController.sendMessage);
+router.post('/send-message-template', verifyToken, MessageController.sendMessageTemplate);
 
 export default router;
